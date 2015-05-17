5d3cb998bc8371e4c04f049b1e4ef9570fd16bfa
Brendan Jones <brendan_kaizen@yahoo.com>
2015-05-16 17:55:58 -0700

    initial commit

---
+++ locations.py
@@ -0,0 +1,17 @@
+from random import seed, choice, randint
+
+seed()
+
+dictionary = {}
+list_of_sources=["locations","titles","adjectives","nouns"]
+for source in list_of_sources:
+    with open ("{}.txt".format(source)) as f:
+        dictionary[source] = f.readlines()
+
+with open ("patterns.txt") as file:
+    patterns = file.readlines()
+
+for i in range(0,20):
+    values = {key: choice(dictionary[key]).strip() for key in dictionary}
+    pattern = choice(patterns)
+    print(pattern.format(**values).strip())
